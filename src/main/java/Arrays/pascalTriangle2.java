a4f557ee1337b9925038f70baa0d0937cca94975
shubhashree <shubhashree.deshpande@clipboardhealth.com>
2022-09-21 16:57:14 +0530

    leet code sep 21

--- src/main/java/Arrays/pascalTriangle2.java
+++ src/main/java/Arrays/pascalTriangle2.java
@@ -3,16 +3,16 @@ package Arrays;
 import java.util.ArrayList;
 import java.util.List;
 
-public class pascalTriangle {
+public class pascalTriangle2 {
 
     public static void main(String[] args) {
-        int numRowa=3;
+        int numRowa=1;
         System.out.println(pascal(numRowa));
     }
 
-    public static List<List<Integer>> pascal(int numRows){
+    public static List<Integer> pascal(int numRows){
        List<List<Integer>> res=new ArrayList<List<Integer>>(numRows);
-       for(int i=1;i<=numRows;i++){
+       for(int i=1;i<=numRows+1;i++){
            List<Integer> row=new ArrayList<>();
            for(int j=0;j<i;j++){
                if(j==0||j==i-1){  //1==3-1 1=2
@@ -25,7 +25,7 @@ public class pascalTriangle {
 
           res.add(row) ;
        }
-       return res;
+       return res.get(numRows);
     }
 
 
